820756e0ed90546a6e3b05d7922c106bdb6434a2
ss <ss>
2025-03-02 15:32:37 +0100

    hello test

--- src/main/java/pl/wsb/FitnessTracker.java
+++ src/main/java/pl/wsb/FitnessTracker.java
@@ -1,5 +1,6 @@
 package pl.wsb;
 
+import com.fasterxml.jackson.core.JsonToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class FitnessTracker {
 
     public static void main(String[] args) {
         SpringApplication.run(FitnessTracker.class, args);
+        System.out.println("hello michał  from tomek");
     }
 
 }
